3d4dcad42f51cda363ff06de34c709526dab377b
ChanWahFung <552095989@qq.com>
2020-04-05 00:47:21 +0800

    :art: .gitignore

--- server/request/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  uid: '5c455fe56fb9a049ef26e4e6',
-  deviceId: 1585305185390,
-  token: 'eyJhY2Nlc3NfdG9rZW4iOiJQemFsbnJ4em1GUUpQNVo4IiwicmVmcmVzaF90b2tlbiI6Ik05d0dyWUpad1VSRUFCSmYiLCJ0b2tlbl90eXBlIjoibWFjIiwiZXhwaXJlX2luIjoyNTkyMDAwfQ=='
-}
