a126f5ea13d967c752f611f46df4787415dd94fd
ChanWahFung <552095989@qq.com>
2020-04-21 23:03:38 +0800

    :sparkles:  book api

---
+++ server/routes/v1/book.js
@@ -0,0 +1,49 @@
+const Router = require('koa-router')
+const router = new Router()
+const request = require('../../request')
+const validator = require('../../middleware/validator')
+const config = require('../../request/config')
+
+/**
+ * 小册类目
+ */
+router.get('/navList', async (ctx, next) => {
+  const options = {
+    utl: 'https://xiaoce-timeline-api-ms.juejin.im/v1/getNavList',
+    method: 'GET'
+  }
+  let { body } = await request(options)
+  ctx.body = body
+})
+
+/**
+ * 小册列表
+ * @param {string} alias - 类目名
+ * @param {number} pageNum
+ */
+router.get('/getListByLastTime', validator({
+  alias: { type: 'string' },
+  pageNum: {
+    type: 'string', 
+    required: true,
+    validator: (rule, value) => Number(value) > 0,
+    message: 'pageNum 需传入正整数'
+  }
+}), async (ctx, next) => {
+  const options = {
+    utl: 'https://xiaoce-timeline-api-ms.juejin.im/v1/getListByLastTime',
+    method: 'GET',
+    params: {
+      client_id: config.deviceId,
+      src: 'web',
+      token: config.token,
+      uid: config.uid,
+      alias: ctx.query.alias || '',
+      pageNum: ctx.query.pageNum || 1
+    }
+  }
+  let { body } = await request(options)
+  ctx.body = body
+})
+
+module.exports = router
