a99dcf26869ad2612c6787cb3c37c98aa36f09f8
chanwahfung <552095989@qq.com>
2020-04-21 18:28:11 +0800

    :sparkles: 小册页template

---
+++ pages/book/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    book detail
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      books: []
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
