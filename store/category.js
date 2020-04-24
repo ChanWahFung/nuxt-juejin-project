20a19cad364f73601fa84c5e7e581005efbf2706
ChanWahFung <552095989@qq.com>
2020-04-25 00:38:53 +0800

    :art: 沸点分类

--- store/category.js
+++ store/category.js
@@ -1,7 +1,37 @@
 export const state = () => ({
-  timelineCategoryList: [], // 首页分类列表
+  timelineCategoryList: [],  // 首页分类列表
   recommendCategoryList: [], // 作者榜单分类列表
-  bookCategoryList: [], // 小册分类列表
+  bookCategoryList: [],     // 小册分类列表
+  topicCategoryList: [      // 沸点分类列表
+    {
+      id: '5c09ea2b092dcb42c740fe73',
+      name: '开源推荐'
+    },
+    {
+      id: '5abb61e1092dcb4620ca3322',
+      name: '内推招聘'
+    },
+    {
+      id: '5abcaa67092dcb4620ca335c',
+      name: '掘金相亲'
+    },
+    {
+      id: '5c106be9092dcb2cc5de7257',
+      name: '上班摸鱼'
+    },
+    {
+      id: '5b514af1092dcb61bd72800d',
+      name: '应用安利'
+    },
+    {
+      id: '5abb67d2092dcb4620ca3324',
+      name: '开发工具'
+    },
+    {
+      id: '5c46a17f092dcb4737217152',
+      name: 'New资讯'
+    },
+  ],
 })
 
 export const mutations = {
