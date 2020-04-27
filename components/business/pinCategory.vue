1433340b1551d6d65fb8286b16df1700d7559bdc
ChanWahFung <552095989@qq.com>
2020-04-27 23:15:19 +0800

    :art: 沸点页分类状态存储

--- components/business/pinCategory.vue
+++ components/business/pinCategory.vue
@@ -7,62 +7,12 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
-  data() {
-    return {
-      navList: [
-        {
-          id: '0',
-          name: '推荐',
-          path: '/pin/recommended'
-        },
-        {
-          id: '1',
-          name: '热门',
-          path: '/pin/hot'
-        },
-        {
-          id: '2',
-          name: '关注',
-          path: '/pin/following'
-        },
-        {
-          id: '5c09ea2b092dcb42c740fe73',
-          name: '开源推荐',
-          path: '/pin/topic/5c09ea2b092dcb42c740fe73'
-        },
-        {
-          id: '5abb61e1092dcb4620ca3322',
-          name: '内推招聘',
-          path: '/pin/topic/5abb61e1092dcb4620ca3322'
-        },
-        {
-          id: '5abcaa67092dcb4620ca335c',
-          name: '掘金相亲',
-          path: '/pin/topic/5abcaa67092dcb4620ca335c'
-        },
-        {
-          id: '5c106be9092dcb2cc5de7257',
-          name: '上班摸鱼',
-          path: '/pin/topic/5c106be9092dcb2cc5de7257'
-        },
-        {
-          id: '5b514af1092dcb61bd72800d',
-          name: '应用安利',
-          path: '/pin/topic/5b514af1092dcb61bd72800d'
-        },
-        {
-          id: '5abb67d2092dcb4620ca3324',
-          name: '开发工具',
-          path: '/pin/topic/5abb67d2092dcb4620ca3324'
-        },
-        {
-          id: '5c46a17f092dcb4737217152',
-          name: 'New资讯',
-          path: '/pin/topic/5c46a17f092dcb4737217152'
-        },
-      ]
-    }
+  computed: {
+    ...mapState('category', {
+      navList: 'pinCategoryList'
+    })
   },
 }
 </script>
@@ -81,7 +31,6 @@ export default {
     justify-content: center;
     font-size: 14px;
     color: #2e3135;
-    padding: 0 12px;
     border-radius: 3px;
     transition: background-color .2s, color .2s;
 
@@ -102,6 +51,10 @@ export default {
     }
     
     >a{
+      width: 100%;
+      line-height: 30px;
+      padding: 0 12px;
+      text-align: center;
       color: #909090;
     }
 
--- store/category.js
+++ store/category.js
@@ -1,7 +1,59 @@
 export const state = () => ({
   timelineCategoryList: [],  // 首页分类列表
   recommendCategoryList: [], // 作者榜单分类列表
-  bookCategoryList: [],     // 小册分类列表
+  bookCategoryList: [],      // 小册分类列表
+  pinCategoryList: [         // 沸点分类列表
+    {
+      id: 'recommended',
+      name: '推荐',
+      path: '/pin/recommended'
+    },
+    {
+      id: 'hot',
+      name: '热门',
+      path: '/pin/hot'
+    },
+    {
+      id: 'following',
+      name: '关注',
+      path: '/pin/following'
+    },
+    {
+      id: '5c09ea2b092dcb42c740fe73',
+      name: '开源推荐',
+      path: '/pin/topic/5c09ea2b092dcb42c740fe73'
+    },
+    {
+      id: '5abb61e1092dcb4620ca3322',
+      name: '内推招聘',
+      path: '/pin/topic/5abb61e1092dcb4620ca3322'
+    },
+    {
+      id: '5abcaa67092dcb4620ca335c',
+      name: '掘金相亲',
+      path: '/pin/topic/5abcaa67092dcb4620ca335c'
+    },
+    {
+      id: '5c106be9092dcb2cc5de7257',
+      name: '上班摸鱼',
+      path: '/pin/topic/5c106be9092dcb2cc5de7257'
+    },
+    {
+      id: '5b514af1092dcb61bd72800d',
+      name: '应用安利',
+      path: '/pin/topic/5b514af1092dcb61bd72800d'
+    },
+    {
+      id: '5abb67d2092dcb4620ca3324',
+      name: '开发工具',
+      path: '/pin/topic/5abb67d2092dcb4620ca3324'
+    },
+    {
+      id: '5c46a17f092dcb4737217152',
+      name: 'New资讯',
+      path: '/pin/topic/5c46a17f092dcb4737217152'
+    },
+  ]
 })
 
 export const mutations = {
