64eeb8369b449d40e59012e1f35c4db2e949d51a
chanwahfung <552095989@qq.com>
2020-05-07 10:21:26 +0800

    :art: 添加组件

--- components/eleComponentsInstall.js
+++ components/eleComponentsInstall.js
@@ -1,10 +1,12 @@
-import { Input, Select, Option, Notification } from 'element-ui'
+import { Input, Button, Select, Option, Notification, Message } from 'element-ui'
 
 export default {
   install(Vue) {
     Vue.use(Input)
     Vue.use(Select)
     Vue.use(Option)
+    Vue.use(Button)
+    Vue.prototype.$message = Message
     Vue.prototype.$notify  = Notification
   }
 }
