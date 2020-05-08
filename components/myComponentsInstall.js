6ddf9908a30fd5337575257e16c64a6c9bcc9b79
chanwahfung <552095989@qq.com>
2020-05-08 16:48:54 +0800

    :art:

--- components/myComponentsInstall.js
+++ components/myComponentsInstall.js
@@ -3,10 +3,8 @@ export default {
     // 批量注册公用组件
     const components = require.context('~/components/common', false, /\.vue$/)
     components.keys().map(path => {
-      const fileName = path.replace(/(.*\/)*([^.]+).*/ig, "$2"); // 获取文件名
-      let conpomentName = fileName.replace(/[A-Z]/g, match => "-" + match.toLowerCase()); // 转小驼峰
-      if (conpomentName[0] === '-') { conpomentName = conpomentName.slice(1) }
-      Vue.component(conpomentName, components(path).default || components(path))
+      const fileName = path.replace(/(.*\/)*([^.]+).*/ig, "$2"); // 获取组件文件名
+      Vue.component(fileName, components(path).default || components(path))
     })
   } 
 }
