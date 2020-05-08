fc0f1fcffa6a1ae4d7e0f7d4272359181fb2147b
chanwahfung <552095989@qq.com>
2020-05-08 15:37:49 +0800

    :art: 图片预览弹窗注册

--- plugins/vue-global.js
+++ plugins/vue-global.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import utils from '~/utils'
 import myComponentsInstall from '~/components/myComponentsInstall'
 import eleComponentsInstall from '~/components/eleComponentsInstall'
-import loginModal from '~/components/common/loginModal/index'
+import loginModal from '~/components/common/loginModal'
+import picturesModal from '~/components/common/picturesModal'
 import '~/assets/scss/element-variables.scss' // elementUI 自定义主题色
 
 Vue.prototype.$utils = utils
 
 Vue.use(loginModal)
+Vue.use(picturesModal)
 Vue.use(myComponentsInstall)
 Vue.use(eleComponentsInstall)
 
